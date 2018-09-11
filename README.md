d2005b527f0b015df58d7140d540ffe8384c3308
Mitchell O'Hair <mitchellohair@Mitchells-MacBook-Pro.local>
2018-09-10 22:38:04 -0700

    Updating README as well as adding comments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # linked-list
-This is a doubly-linked list project in C++ without STL containers for storing school class details i.e. class name, instructor name, grade etc. It also implements bubble sort and merge sort on stored list.
+This is a doubly-linked list project in C++ without STL containers for storing school class details i.e. class name, instructor name, grade etc. It also implements bubble sort and merge sort on the stored list.
--- fileRead.cpp
+++ fileRead.cpp
@@ -34,42 +34,55 @@ void fileRead(slist*& header) {
       }
       node-> course = new record;
 
+      // Course Term
       getline(fin, node->course->term);
       node->course->term = (node->course->term.empty()) ? "N/A"
                                                         : node->course->term;
+      // Course Title
       getline(fin, node->course->title);
       node->course->title = (node->course->title.empty()) ? "N/A"
                                                           : node->course->title;
+      // Course Description
       getline(fin, node->course->description);
       node->course->description = (node->course->description.empty()) ? "N/A"
                                                                       : node->course->description;
+      // Course Ticket Number
       getline(fin, node->course->ticket);
       node->course->ticket = (node->course->ticket.empty()) ? "N/A"
                                                             : node->course->ticket;
+      // Course Location
       getline(fin, node->course->location);
       node->course->location = (node->course->location.empty()) ? "N/A"
                                                                 : node->course->location;
+      // Course Lecture Time
       getline(fin, node->course->lecture);
       node->course->lecture = (node->course->lecture.empty()) ? "N/A"
                                                               : node->course->lecture;
+      // Course Instructor
       getline(fin, node->course->instructor);
       node->course->instructor = (node->course->instructor.empty()) ? "N/A"
                                                                     : node->course->instructor;
+      // Course Lab Location
       getline(fin, node->course->labLocation);
       node->course->labLocation = (node->course->labLocation.empty()) ? "N/A"
                                                                       : node->course->labLocation;
+      // Course Lab Time
       getline(fin, node->course->lab);
       node->course->lab = (node->course->lab.empty()) ? "N/A"
                                                       : node->course->lab;
+      // Course Lab Instructor
       getline(fin, node->course->labInstructor);
       node->course->labInstructor = (node->course->labInstructor.empty()) ? "N/A"
                                                                           : node->course->labInstructor;
+      // Course Units
       getline(fin, node->course->units);
       node->course->units = (node->course->units.empty()) ? "N/A"
                                                           : node->course->units;
+      // Course Prereqs
       getline(fin, node->course->prereqs);
       node->course->prereqs = (node->course->prereqs.empty()) ? "N/A"
                                                               : node->course->prereqs;
+      // Course Grade
       getline(fin, node->course->grade);
       node->course->grade = (node->course->grade.empty()) ? "N/A"
                                                           : node->course->grade;
